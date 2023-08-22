ddc7bee670835cf26cd7a3440f3fe49903d64375
MRosama90 <mrizk15903@gmail.com>
2023-08-22 15:27:52 -0700

    asd

--- Test/main.h
+++ Test/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
+#include <stdlib.h>
+
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
