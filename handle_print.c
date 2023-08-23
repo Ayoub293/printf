9c67b65acd9993759e05ac47f5cf11074c2f3a89
Ayoub293 <ahmedabuayub2@gmail/com>
2023-08-22 17:43:13 -0700

    update

--- handle_print.c
+++ handle_print.c
@@ -1,20 +1,18 @@
 #include "main.h"
-
 /**
- * handle_print - asdasd
- * @fmt: asdasd
- * @list: asdasd
- * @ind: asdasd
- * @buffer: asdasd
- * @flags: asdasd
- * @width: asdasd
- * @precision: asdasd
- * @size: asdasd
- * Return: asdasd
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
-
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -27,6 +25,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
